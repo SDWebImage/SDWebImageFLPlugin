d401c24346400dcaf674a48f65794e0d00536d1a
DreamPiggy <lizhuoli1126@126.com>
2018-08-31 18:17:28 +0800

    Update the test, support codecov report

--- Example/Tests/SDWebCacheCategoriesTests.m
+++ Example/Tests/SDWebCacheCategoriesTests.m
@@ -33,5 +33,46 @@
     [self waitForExpectationsWithCommonTimeout];
 }
 
+- (void)testFLAnimatedImageViewSetImageWithGIFPlaceholder {
+    XCTestExpectation *expectation = [self expectationWithDescription:@"FLAnimatedImageView setImageWithURL with GIF placeholder"];
+    
+    FLAnimatedImage *animatedImage = [FLAnimatedImage animatedImageWithGIFData:[self testGIFData]];
+    expect(animatedImage).toNot.beNil();
+    SDFLAnimatedImage *placeholderImage = [[SDFLAnimatedImage alloc] initWithAnimatedImage:animatedImage];
+    expect(placeholderImage).toNot.beNil();
+    
+    FLAnimatedImageView *imageView = [[FLAnimatedImageView alloc] init];
+    NSURL *originalImageURL = nil; // invalid URL
+    [imageView sd_setImageWithURL:originalImageURL placeholderImage:placeholderImage completed:^(UIImage * _Nullable image, NSError * _Nullable error, SDImageCacheType cacheType, NSURL * _Nullable imageURL) {
+        expect(error).notTo.beNil();
+        FLAnimatedImage *currentAnimatedImage = imageView.animatedImage;
+        expect(currentAnimatedImage).equal(placeholderImage.animatedImage);
+        
+        [expectation fulfill];
+    }];
+    [self waitForExpectationsWithCommonTimeout];
+}
+
+// Since `SDFLAnimatedImage` confroms to `SDAnimatedImage` protocol, it should be compatible for `SDAnimatedImageView` rendering. Maybe this may be changed in the future but currently add this test as well.
+- (void)testSDFLAnimatedImageWorksForSDAnimatedImageView {
+    SDAnimatedImageView *imageView = [SDAnimatedImageView new];
+    SDFLAnimatedImage *image = [SDFLAnimatedImage imageWithData:[self testGIFData]];
+    imageView.image = image;
+    expect(imageView.image).notTo.beNil();
+    expect(imageView.currentFrame).notTo.beNil(); // current frame
+}
+
+#pragma mark - Util
+
+- (NSString *)testGIFPath {
+    NSBundle *testBundle = [NSBundle bundleForClass:[self class]];
+    return [testBundle pathForResource:@"TestImage" ofType:@"gif"];
+}
+
+- (NSData *)testGIFData {
+    NSData *testData = [NSData dataWithContentsOfFile:[self testGIFPath]];
+    return testData;
+}
+
 @end
 
